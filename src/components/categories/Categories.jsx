6de88d054c41ee859b72570c7a9d0a19c947c585
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-14 06:26:16 -0400

    created categories component

---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+const Categories = () => {
+  return (
+    <>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> algorithms
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> angular
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> animation
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> blockchain
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> business
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> expressjs
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> games
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> github
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> illustrator
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> ionic
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> javascript
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> mobile
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> nodejs
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> photoshop
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> polymer
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> react
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> self
+      </Button>
+      <Button pill variant="light" className="mr-3 mb-3">
+        <i class="fas fa-code"></i> wordpress
+      </Button>
+    </>
+  );
+};
+
+export default Categories;
