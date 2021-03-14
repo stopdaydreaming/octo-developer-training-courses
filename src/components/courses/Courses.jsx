bb45bd536d98b822a03ed622c921675becb1f9f7
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-14 15:28:13 -0400

    setup courses mapping

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -1,64 +1,45 @@
-import React, { Component } from "react";
-import CourseRow from "../courseRow/CourseRow";
+import React from "react";
 
-import data from '../../data/courses.json';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
 
-class Courses extends Component {
-  state = {
-    courses: [],
-    filteredCourses: [],
-  };
+import courses from "../../data/courses.js";
 
-  componentDidMount() {
-    fetch(data)
-      .then(res => res.text())
-      .then(text => {
-        console.log('data:', data);
-        console.log('data:', data.courses[0].coursename);
-        console.log('data:', data.courses[0].courselink);
-        console.log('data:', data.courses[0].category);
-        // const courses = data.results.map(result => {
-        //   const course = {
-        //     title: result.coursename,
-        //     link: result.courselink,
-        //     category: result.category
-        //   };
-        //   return course;
-        // });
-        // this.setState({ courses, filteredCourses: courses });
-      })
-      .catch(error => console.log(error));
-  }
+const Courses = () => {
+  return (
+    <div className="mt-5">
+      {/* // if course is js, give it js badge */}
+      <h1>Currently Enrolled Courses</h1>
 
-  render() {
-    return (
-      <div className="mt-5">
-        {/* // if course is js, give it js badge */}
-        <h1>Currently Enrolled Courses</h1>
-
-        <div className="row">
-          <div className="col">
-            <table className="table">
-              <thead>
-                <tr>
-                  {/* <th scope="col">#</th> */}
-                  <th scope="col">Title</th>
-                  <th scope="col">Tags</th>
-                  <th scope="col">View</th>
+      <Row>
+        <Col>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Tags</th>
+                <th scope="col">View</th>
+              </tr>
+            </thead>
+            <tbody>
+              {courses.map((course, index) => (
+                <tr key={index}>
+                  <td>{course.coursename}</td>
+                  <td>{course.category}</td>
+                  <td>
+                    <a href={course.courselink}>
+                      <button className="btn-tomato">Course</button>
+                    </a>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {this.state.courses.map(course => (
-                  <CourseRow {...course} />
-                //   <CourseRow {...course} key={index} />
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default Courses;
