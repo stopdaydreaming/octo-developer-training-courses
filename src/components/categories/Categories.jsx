bc120cd2b9af1c74a3541e6786b5bb0d50df1f23
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-14 07:01:37 -0400

    remove pill from button

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -4,59 +4,59 @@ import Button from "react-bootstrap/Button";
 const Categories = () => {
   return (
     <>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> algorithms
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> algorithms
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> angular
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> angular
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> animation
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> animation
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> blockchain
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> blockchain
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> business
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> business
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> expressjs
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> expressjs
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> games
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> games
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> github
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> github
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> illustrator
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> illustrator
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> ionic
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> ionic
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> javascript
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> javascript
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> mobile
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> mobile
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> nodejs
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> nodejs
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> photoshop
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> photoshop
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> polymer
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> polymer
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> react
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> react
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> self
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> self
       </Button>
-      <Button pill variant="light" className="mr-3 mb-3">
-        <i class="fas fa-code"></i> wordpress
+      <Button variant="light" className="mr-3 mb-3">
+        <i className="fas fa-code"></i> wordpress
       </Button>
     </>
   );
