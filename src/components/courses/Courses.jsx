a1117e69d4ba348d090ae43d9c9a36db295e0b70
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-14 06:25:52 -0400

    created courses component

---
+++ src/components/courses/Courses.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Courses = () => {
+    return (
+        <div className="mt-5">
+            {/* // if course is js, give it js badge */}
+            <h1>Currently Enrolled Courses</h1>
+        </div>
+    );
+};
+
+export default Courses;
