a5e1164340c7b365b55db3d249d674da8dc0b45a
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-14 06:27:22 -0400

    added components to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import Categories from "./components/categories/Categories";
+import Navigation from "./components/navigation/Navigation";
+import Courses from "./components/courses/Courses";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Container>
+        <Row>
+          <Col>
+            <Categories />
+            <Courses />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
