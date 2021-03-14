ec87a5479aedfe8aa15f3489877f0199fb8ec436
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-14 06:25:37 -0400

    created navigation component

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Navbar from "react-bootstrap/Navbar";
+
+const Navigation = () => {
+  return (
+    <>
+      <Navbar expand="lg" bg="dark" variant="dark" className="mb-5">
+        <Navbar.Brand href="#home">
+            <h3><i class="fas fa-laptop-code"></i> Developer Online Training </h3>
+            </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Created By: <a href="#login">Lisa Copeland</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+};
+
+export default Navigation;
